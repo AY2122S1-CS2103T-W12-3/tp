9572ddbea1bca9b58d7b0057745ab1c1c402f42d
bingcheng45 <bingcheng45@gmail.com>
2021-09-28 01:36:21 +0800

    Fix checkstyle & gradle built successfully

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructor for Remark
+     *
+     * @param remark takes in a strings as the remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
